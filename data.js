56a4a2c21a1f3b83a7334fea2908eb8b44e55492
Ibrohim-dev17 <ibrohimnabixonov17@gmail.com>
2026-08-19 13:29:07 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,24 +1,17 @@
-// Backend server manzili (agar loyiha kompyuterda ishlayotgan bo'lsa)
-const API_URL = "http://localhost:3000";
-
 let allProducts = [];
 
-// Serverdan mahsulotlarni internet orqali tortib kelish
 async function fetchProducts() {
   try {
     let response = await fetch(`${API_URL}/api/products`);
     allProducts = await response.json();
     
-    // Mahsulotlar kelgandan keyin sahifadagi asosiy funksiyani ishga tushiramiz
-    if (typeof renderFavorites === 'function') renderFavorites();
-    if (typeof renderCart === 'function') renderCart();
+    // Serverdan mahsulotlar kelgandan keyin ularni ekranga chiqarish funksiyalari:
     if (typeof renderHomeProducts === 'function') renderHomeProducts();
-    if (typeof loadProductDetails === 'function') loadProductDetails(); // Agar mahsulot tafsilotlari sahifasi bo'lsa
+    if (typeof renderProducts === 'function') renderProducts(); // Ba'zi loyihalarda shunday nomlanadi
     
   } catch (err) {
     console.error("Serverdan mahsulotlarni olib kelishda xatolik:", err);
   }
 }
 
-// Sahifa ochilishi bilan ma'lumotlarni yuklaymiz
 fetchProducts();
